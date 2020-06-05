1d21f79c2fdd7462374200b0244beab618d566bb
legaccy <66487398+legaccy@users.noreply.github.com>
2020-06-05 20:06:15 +0200

    Create console.js

---
+++ handlers/console.js
@@ -0,0 +1,7 @@
+module.exports = (bot) => {
+let prompt = process.openStdin()
+prompt.addListener("data", res => {
+    let x = res.toString().trim().split(/ +/g)
+        bot.channels.get("CHANNEL-ID").send(x.join(" "));
+    });
+}
